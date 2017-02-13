62c16dedcdae48157f0a05bca6645a6f54159c6b
jorge <jorge@example.com>
2017-02-13 09:14:25 -0500

    0.02 Se agrega gridview para la visualizacion de los iconos

--- app/src/main/java/com/kdev/katie/HomeActivityFragment.java
+++ app/src/main/java/com/kdev/katie/HomeActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +18,12 @@ public class HomeActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview_home);
+        gridView.setAdapter(new ImageAdapter(getContext()));
+
+        return rootView;
     }
 }
