a8e705f4d5ad8a972bc33aa3864f82459edc5091
jorge <jorge@example.com>
2017-03-14 18:54:19 -0500

    0.11 Actualizacion del gridview para mostrar el icono y texto

--- app/src/main/java/com/kdev/katie/ImageAdapter.java
+++ app/src/main/java/com/kdev/katie/ImageAdapter.java
@@ -1,11 +1,12 @@
 package com.kdev.katie;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by jorge alberto on 13/02/2017.
@@ -36,19 +37,31 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
+
         if (convertView == null) {
-            // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(195, 195));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(24, 16, 16, 16);
-        } else {
-            imageView = (ImageView) convertView;
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.icon_spec, parent, false);
         }
 
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
+        ImageView icon = (ImageView) convertView.findViewById(R.id.imageView);
+        TextView text = (TextView) convertView.findViewById(R.id.textView2);
+        icon.setImageResource(mThumbIds[position]);
+        text.setText("Test!");
+//        ImageView imageView;
+//        if (convertView == null) {
+//            // if it's not recycled, initialize some attributes
+//            imageView = new ImageView(mContext);
+//            imageView.setLayoutParams(new GridView.LayoutParams(195, 195));
+//            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//            imageView.setPadding(24, 16, 16, 16);
+//        } else {
+//            imageView = (ImageView) convertView;
+//        }
+//
+//        imageView.setImageResource(mThumbIds[position]);
+//        return imageView;
+
+        return convertView;
     }
 
     // references to our images
