e9050693df82b5449567e55c7abbce6d927e16fb
jorge <jorge@example.com>
2017-03-21 10:32:05 -0500

    0.20 Load Activity ahora lanza la fullscreen activity y no deja registro para volver atras

--- app/src/main/java/com/kdev/katie/LoadActivity.java
+++ app/src/main/java/com/kdev/katie/LoadActivity.java
@@ -1,5 +1,6 @@
 package com.kdev.katie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -36,5 +37,7 @@ public class LoadActivity extends AppCompatActivity {
 
     public void hideProgress(View v){
         spinner.setVisibility(View.GONE);
+        Intent i = new Intent(this, FullscreenActivity.class);
+        startActivity(i);
     }
 }
