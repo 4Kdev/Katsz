c16254ca2537ff7323157ece1a3105459854b053
jorge <jorge@example.com>
2017-03-25 17:39:56 -0500

    0.35 arregla boton y se have timer en load activity

--- app/src/main/java/com/kdev/katie/LoadActivity.java
+++ app/src/main/java/com/kdev/katie/LoadActivity.java
@@ -4,13 +4,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class LoadActivity extends AppCompatActivity {
     private ProgressBar spinner;
+    private FragmentManager fm;
+    private LoadActivityFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,9 @@ public class LoadActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         spinner = (ProgressBar)findViewById(R.id.progressBar);
+
+        fm = getSupportFragmentManager();
+        fragment = (LoadActivityFragment) fm.findFragmentById(R.id.load_fragment);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,13 +38,14 @@ public class LoadActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    public void showProgress(View v){
-        spinner.setVisibility(View.VISIBLE);
-    }
+//    public void showProgress(View v){
+//        spinner.setVisibility(View.VISIBLE);
+//    }
 
-    public void hideProgress(View v){
-        spinner.setVisibility(View.GONE);
-        Intent i = new Intent(this, WebActivity.class);
-        startActivity(i);
-    }
+//    public void hideProgress(View v){
+//        Toast.makeText(this, "See ad's" , Toast.LENGTH_SHORT).show();
+//        fragment.cancelTimer();
+//        Intent i = new Intent(this, WebActivity.class);
+//        startActivity(i);
+//    }
 }
--- app/src/main/java/com/kdev/katie/LoadActivityFragment.java
+++ app/src/main/java/com/kdev/katie/LoadActivityFragment.java
@@ -1,22 +1,69 @@
 package com.kdev.katie;
 
+import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class LoadActivityFragment extends Fragment {
+    private TextView textCountdown;
+    public CountDownTimer counter;
 
     public LoadActivityFragment() {
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_load, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_load, container, false);
+        textCountdown = (TextView) rootView.findViewById(R.id.text_load);
+        Button b = (Button) rootView.findViewById(R.id.button_load_web);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "See ad's" , Toast.LENGTH_SHORT).show();
+                cancelTimer();
+                skipThis();
+            }
+        });
+
+        timerCount();
+
+        return rootView;
+    }
+
+    public boolean timerCount(){
+        counter = new CountDownTimer(5000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                textCountdown.setText("" + millisUntilFinished / 1000);
+            }
+
+        public void onFinish() {
+            textCountdown.setText("done!");
+            skipThis();
+        }
+        }.start();
+        return false;
+    }
+
+    public void cancelTimer(){
+        counter.cancel();
+    }
+
+    public void skipThis(){
+        Intent i = new Intent(getContext(), WebActivity.class);
+        startActivity(i);
     }
 }
