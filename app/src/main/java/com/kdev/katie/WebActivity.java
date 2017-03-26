3365264a1d79b606774ce3c7ac59c711c943b35c
jorge <jorge@example.com>
2017-03-26 09:18:10 -0500

    0.37 Se arregla hide en webView

--- app/src/main/java/com/kdev/katie/WebActivity.java
+++ app/src/main/java/com/kdev/katie/WebActivity.java
@@ -80,6 +80,9 @@ public class WebActivity extends AppCompatActivity {
             }
         });
 
+        //Oculta bar navigation luego de que se active el keyboard
+        UiChangeListener();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_back);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -166,5 +169,24 @@ public class WebActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         delayedHide(1000);
+    }
+
+    public void UiChangeListener()
+    {
+        final View decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener (new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(
+                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                }
+            }
+        });
     }
 }
